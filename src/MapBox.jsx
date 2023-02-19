336b9c96253a14bde48a7c6124fc27475e2cc53f
Patrick Goulding <patrickgoulding@gmail.com>
2023-02-19 15:42:28 -0800

    Fix styling issue at smaller screen size

--- src/MapBox.jsx
+++ src/MapBox.jsx
@@ -5,7 +5,10 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import EmailIcon from '@mui/icons-material/Email';
 import ShareIcon from '@mui/icons-material/Share';
+import EditLocationOutlinedIcon from '@mui/icons-material/EditLocationOutlined';
+import RestartAltOutlinedIcon from '@mui/icons-material/RestartAltOutlined';
 
+import loadingMap from './mgpt_loading.gif'
 import miniMap from './example-mini-map.jpeg'
 const MapBox = ({idea, toggleShowForm}) => {
 
@@ -14,22 +17,28 @@ const MapBox = ({idea, toggleShowForm}) => {
         container 
         spacing={2}
         justifyContent="center"
-        alignItems="flex-start" 
+        alignItems="flex-start"
         >
             <Grid item xs={10} md={6}>
-                <Box sx={{bgcolor: '#cfe8fc', minHeight:"400px"}}>
+                <Box sx={{bgcolor: '#cfe8fc', minHeight:"400px", boxShadow:"3px 3px 5px 0px rgba(0,0,0,0.75)"}}>
                     <Box sx={{backgroundColor:'lightblue', height:'380px'}}>
+
+                        <img style={{width:"100%"}} src={loadingMap} alt="loading map"/>
                     {/* Display the generated map here */}
                     </Box>
-                    <div style={{display:"flex", justifyContent:"space-around"}}>
+                    <div style={{display:"flex", justifyContent:"space-around", padding:"5px"}}>
                         <Button
+                        sx={{margin:"10px"}}
                         className="submitButton">
+                            <EditLocationOutlinedIcon/>
                             Make Edits & add more points
                         </Button>
                         <Button
                             className="submitButton"
                             onClick={() => toggleShowForm(true)}
+                            sx={{margin:"10px"}}
                             >
+                                <RestartAltOutlinedIcon/>
                             Reset and try again
                         </Button>
                     </div>
