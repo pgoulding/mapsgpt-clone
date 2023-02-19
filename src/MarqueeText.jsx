b3607a58154f7c177f1fdeaf7ba365c60abb0d0a
Patrick Goulding <patrickgoulding@gmail.com>
2023-02-19 15:28:07 -0800

    Add func for passing idea to form, and some fancy css for text hover

--- src/MarqueeText.jsx
+++ src/MarqueeText.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Fade, Typography } from "@mui/material";
 import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import './MarqueeText.css'
 
-const MarqueeText = ({ textArray }) => {
-  // const classes = useStyles();
+const MarqueeText = ({ textArray, selectIdea }) => {
+
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
+  const [lightFilled, setLightFilled] = useState(false)
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -21,22 +24,32 @@ const MarqueeText = ({ textArray }) => {
 
   const handlePause = () => {
     setIsPaused(true);
+    setLightFilled(true);
   };
 
   const handleResume = () => {
     setIsPaused(false);
+    setLightFilled(false)
   };
+
+  const handleIdea = (idea) => {
+    //maybe something to populate the idea into the form input?
+    console.log('MarqueeText', idea)
+    selectIdea(idea)
+  }
 
   return (
     <div
       onMouseEnter={handlePause}
       onMouseLeave={handleResume}
+      onClick={() => handleIdea(textArray[currentTextIndex])}
       style={{margin:'auto', display:'flex'}}
-
     >
       <Fade in={true} timeout={1000}>
-        
-        <Typography><LightbulbOutlinedIcon/> {textArray[currentTextIndex]}</Typography>
+
+          
+          <Typography className="marqueeText"> {lightFilled ?  <LightbulbIcon/> : <LightbulbOutlinedIcon />} {textArray[currentTextIndex]}</Typography>
+
       </Fade>
     </div>
   );
